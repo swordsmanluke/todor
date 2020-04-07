61c4cb292e30ef87045a2d4cd08c8ad4a0534fbc
Lucas Taylor <lucas@perfectlunacy.com>
2020-04-06 20:39:47 -0700

    Colors ScheduledItems

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate serde_derive;
 
 #[macro_use] extern crate lazy_static;
 extern crate regex;
+extern crate colored; // not needed in Rust 2018
 
 // use crate::google_scheduler::*;
 use crate::schedule_formatter::*;
@@ -10,10 +11,12 @@ use crate::todoist_scheduler::*;
 use crate::todoist_client::*;
 use std::fs::File;
 use crate::schedule_trait::ScheduledItem;
+use crate::schedule_colorer::color_item;
 
 // mod google_scheduler;
 mod schedule_trait;
 mod schedule_formatter;
+mod schedule_colorer;
 mod todoist_scheduler;
 mod todoist_client;
 
@@ -28,7 +31,7 @@ fn main() {
     for sched in tds.get_schedule() {
         for item in sched.iter().collect::<Vec<&ScheduledItem>>() {
             match format_item(item, 20) {
-                Some(s) => println!("{}", s),
+                Some(s) => println!("{}", color_item(item, &s)),
                 None => {}
             }
         }
---
+++ src/schedule_colorer.rs
@@ -0,0 +1,71 @@
+use crate::schedule_trait::ScheduledItem;
+use chrono::Local;
+use colored::*;
+
+
+const THREE_MIN_TIL: i64 = 3 * 60 * 1000;
+const CRITICAL_WARNING_STARTS: i64 = THREE_MIN_TIL - 1;
+const TEN_MIN_TIL: i64 = 10 * 60 * 1000;
+const STARTED: i64 = 1;
+const ONE_MINUTE_PAST: i64 = -60 * 1000;
+const ETERNITY: i64 = 9223372036854775807; // max_val, basically.
+
+pub fn color_item(item: &ScheduledItem, text: &String) -> String {
+    let time_remaining = (Local::now() - item.time).num_milliseconds();
+    color_item_with_time_remaining(text, time_remaining)
+}
+
+fn color_item_with_time_remaining(text: &String, time_remaining: i64) -> String {
+    // All of these ranges are backwards, since we're looking at time remaining.
+    // If you're 1 minute past, that's -60 seconds remaining.
+    // If you're ten minutes 'til, that's 600 seconds remaining.
+    // So, the ranges are out of order, but that's ok. It all works out right in the end.
+    match time_remaining {
+        ONE_MINUTE_PAST..=CRITICAL_WARNING_STARTS => text.red().bold().to_string(),
+        THREE_MIN_TIL..=TEN_MIN_TIL => text.yellow().to_string(),
+        _  => text.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::TimeZone;
+
+    #[test]
+    fn when_more_than_ten_minutes_out_text_is_plain() {
+        let colored = color_item_with_time_remaining(&"No rush".to_string(), TEN_MIN_TIL + 1);
+        assert_eq!("No rush", colored);
+    }
+
+    #[test]
+    fn when_within_ten_minutes_text_turns_yellow() {
+        let colored = color_item_with_time_remaining(&"Almost Time".to_string(), TEN_MIN_TIL - 1);
+        assert_eq!("Almost Time".yellow().to_string(), colored);
+    }
+
+    #[test]
+    fn when_within_three_minutes_text_turns_yellow() {
+        let colored = color_item_with_time_remaining(&"HURRY!".to_string(), THREE_MIN_TIL - 1);
+        assert_eq!("HURRY!".red().bold().to_string(), colored);
+    }
+
+    #[test]
+    fn when_within_one_minute_past_text_is_red() {
+        let colored = color_item_with_time_remaining(&"On your way, I hope!".to_string(), ONE_MINUTE_PAST + 1);
+        assert_eq!("On your way, I hope!".red().bold().to_string(), colored);
+    }
+
+    #[test]
+    fn when_more_than_one_minute_past_text_is_plain() {
+        let colored = color_item_with_time_remaining(&"Hope you're there".to_string(), ONE_MINUTE_PAST - 1);
+        assert_eq!("Hope you're there", colored);
+    }
+
+    fn item() -> ScheduledItem {
+        let scheduled_time = Local.ymd(2020, 4, 2).and_hms(12, 1, 13);
+        let location = Some("location".to_string());
+        ScheduledItem::new("A meeting".to_string(), scheduled_time, location)
+    }
+
+}
